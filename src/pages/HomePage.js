885d217491d8a7147dd486d77415d59eee147b8f
Borious <borisemilov@yahoo.com>
2022-03-13 18:13:52 +0200

    first change to my new brach for testing

---
+++ src/pages/HomePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Greeting } from '../Greeting'
+
+export const HomePage = () => (
+  <>
+  <h1>Home Page</h1>
+  <Greeting name="Boris" numberOfMessages={95}/>
+  </>
+
+)
